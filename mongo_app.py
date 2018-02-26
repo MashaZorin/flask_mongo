d6bbe894a065e0ba662b515d5561b492d6ef2ec9
JFreud <jrm.frdnbrg@gmail.com>
2018-02-26 17:56:06 -0500

    taking forever?

--- mongo_app.py
+++ mongo_app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, redirect, url_for, session, flash
-import pymongo, requests
+import pymongo, requests, random
 
 
 mongo_app = Flask(__name__)
 
 connection = pymongo.MongoClient("homer.stuy.edu")
+connection.drop_database("freudenbergJ-zorinM")
 client = connection["freudenbergJ-zorinM"]
-client.drop_database("freudenbergJ-zorinM")
 # client.drop_database("freudenbergJ-zorinM")
 movies = client["movies"]
 
 r = requests.get("https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json")
 data = r.json()
+
 for doc in data:
     movies.insert_one(doc)
 
@@ -26,7 +27,8 @@ def answer():
     L = list()
     for movie in direct_cursor:
         L.append(movie)
-    return L
+    flash(random.choice(L))
+    return render_template("base.html")
 
 
 if __name__ == "__main__":
